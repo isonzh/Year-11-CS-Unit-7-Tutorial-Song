eea0b5106ccbd1023636431a4ba53a9627df50a2
isonzh <isonz2025@student.cis.edu.hk>
2023-03-30 12:25:42 +0800

    hi

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+        //properties
+        private String title;
+        private String artist;
+    ArrayList<String> temp = new ArrayList<>();
+
+        //constructor
+        public Song(String title, String artist){
+            this.title = title;
+            this.artist = artist;
+        }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+    public int howMany (ArrayList<String> people){
+        int count = 0;
+        for (String name: people) {
+            String finalName = name.toLowerCase();
+            if (temp.contains(finalName) == false) {
+                temp.add(finalName);
+                count++;
+            }
+
+        }
+        return count;
+
+    }
 }
